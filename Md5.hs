72e1fc54bf768fc8053816be718716676767cfee
max <xalyes@gmail.com>
2020-06-17 22:17:44 +0300

    Some reorganizations in sequence of code blocks

--- Md5.hs
+++ Md5.hs
@@ -17,53 +17,11 @@ data Byte64 = Byte64 Byte32 Byte32 deriving Show
 
 zeroByte = Byte False False False False False False False False
 
-bools8ToByte :: [Bool] -> Byte
-bools8ToByte x = let
-    bit0 = x!!7
-    bit1 = x!!6
-    bit2 = x!!5
-    bit3 = x!!4
-    bit4 = x!!3
-    bit5 = x!!2
-    bit6 = x!!1
-    bit7 = x!!0
-  in Byte bit7 bit6 bit5 bit4 bit3 bit2 bit1 bit0
-
-bytesToBools :: [Byte] -> [Bool]
-bytesToBools [] = []
-bytesToBools (Byte bit7 bit6 bit5 bit4 bit3 bit2 bit1 bit0 : xs) = [bit7, bit6, bit5, bit4, bit3, bit2, bit1, bit0] ++ bytesToBools xs
-
-bools32ToByte32 :: [Bool] -> Byte32
-bools32ToByte32 x = let
-    byte0 = bools8ToByte (drop 24 x)
-    byte1 = bools8ToByte (take 8 (drop 16 x))
-    byte2 = bools8ToByte (take 8 (drop 8 x))
-    byte3 = bools8ToByte (take 8 (x))
-  in Byte32 byte3 byte2 byte1 byte0
-
-word8ToByte :: Word8 -> Byte
-word8ToByte x = bools8ToByte $ toBits x
-
-intToByte32 :: Int -> Byte32
-intToByte32 x = bools32ToByte32 $ toBits x
-
-integerToByte64 :: Integer -> Byte64
-integerToByte64 x = let
-    bools = toBits x
-    small32 = bools32ToByte32 (drop 32 bools)
-    big32 = bools32ToByte32 (take 32 bools)
-  in Byte64 big32 small32
-
-bitOpByte :: (Bool -> Bool -> Bool) -> Byte -> Byte -> Byte
-bitOpByte op (Byte bit7 bit6 bit5 bit4 bit3 bit2 bit1 bit0) 
-    (Byte bit7' bit6' bit5' bit4' bit3' bit2' bit1' bit0')
-        = (Byte (bit7 `op` bit7') (bit6 `op` bit6') (bit5 `op` bit5') (bit4 `op` bit4') (bit3 `op` bit3') (bit2 `op` bit2') (bit1 `op` bit1') (bit0 `op` bit0'))
-
 md5 :: String -> String
 md5 str = let
-        (a, b, c, d) = md5Internal (splitEvery 32 (bytesToBools $ prepareString str)) (toBits a00, toBits b00, toBits c00, toBits d00)
-        result = ([showHex' (fromBits i) | i <- reverse $ splitEvery 8 a] ++ [showHex' (fromBits i) | i <- reverse $ splitEvery 8 b]
-         ++ [showHex' (fromBits i) | i <- reverse $ splitEvery 8 c] ++ [showHex' (fromBits i) | i <- reverse $ splitEvery 8 d])
+        (a, b, c, d) = md5Internal ((bytesToBytes32 $ prepareString str)) (intToByte32 a00, intToByte32 b00, intToByte32 c00, intToByte32 d00)
+        result = ([showHex' (fromBits i) | i <- reverse $ splitEvery 8 $ bytesToBools $ byte32ToBytes a] ++ [showHex' (fromBits i) | i <- reverse $ splitEvery 8 $ bytesToBools $ byte32ToBytes b]
+         ++ [showHex' (fromBits i) | i <- reverse $ splitEvery 8 $ bytesToBools $ byte32ToBytes c] ++ [showHex' (fromBits i) | i <- reverse $ splitEvery 8 $ bytesToBools $ byte32ToBytes d])
     in foldl (.) ("" ++) result $ ""
 
 prepareString :: String -> [Byte]
@@ -74,15 +32,7 @@ prepareString str =
     in
         (addLength len . alignBy448 . convertBytesToLittleEndian . addPadding) inputStringInBytes
 
-md5Step :: State ([[Bool]], Int, [Bool], [Bool], [Bool], [Bool]) ()
-md5Step = state $ \(x, i, a, b, c, d) -> ((), (x, i+1, d, (work x a b c d (s!!i) i), b, c)) where
-    work x a b c d s i | i < 0 || i > 63 = error "Expected i from 0 to 63"
-                   | i < 16 = b |+ (shiftLMultiple (a |+ (funF b c d) |+ (x!!i) |+ (t!!i)) s)
-                   | i < 32 = b |+ (shiftLMultiple (a |+ (funG b c d) |+ ((x!!((5*i + 1) `mod` 16)) |+ (t!!i))) s)
-                   | i < 48 = b |+ (shiftLMultiple (a |+ (funH b c d) |+ ((x!!((3*i + 5) `mod` 16)) |+ (t!!i))) s)
-                   | i < 64 = b |+ (shiftLMultiple (a |+ (funI b c d) |+ ((x!!((7*i) `mod` 16)) |+ (t!!i))) s)
-
-md5Internal :: [[Bool]] -> ([Bool], [Bool], [Bool], [Bool]) -> ([Bool], [Bool], [Bool], [Bool])
+md5Internal :: [Byte32] -> (Byte32, Byte32, Byte32, Byte32) -> (Byte32, Byte32, Byte32, Byte32)
 md5Internal input (a0, b0, c0, d0) =
   let
     (_, _, a, b, c, d) = snd $ runState (replicateM_ 64 md5Step) (input, 0, a0, b0, c0, d0)
@@ -91,6 +41,14 @@ md5Internal input (a0, b0, c0, d0) =
     if length input == 16
     then stepResult
     else md5Internal (drop 16 input) stepResult
+
+md5Step :: State ([Byte32], Int, Byte32, Byte32, Byte32, Byte32) ()
+md5Step = state $ \(x, i, a, b, c, d) -> ((), (x, i+1, d, (work x a b c d (s!!i) i), b, c)) where
+    work x a b c d s i | i < 0 || i > 63 = error "Expected i from 0 to 63"
+                   | i < 16 = b |+ (shiftLMultiple (a |+ (funF b c d) |+ (x!!i) |+ (intToByte32 (t!!i))) s)
+                   | i < 32 = b |+ (shiftLMultiple (a |+ (funG b c d) |+ ((x!!((5*i + 1) `mod` 16)) |+ (intToByte32 (t!!i)))) s)
+                   | i < 48 = b |+ (shiftLMultiple (a |+ (funH b c d) |+ ((x!!((3*i + 5) `mod` 16)) |+ (intToByte32 (t!!i)))) s)
+                   | i < 64 = b |+ (shiftLMultiple (a |+ (funI b c d) |+ ((x!!((7*i) `mod` 16)) |+ (intToByte32 (t!!i)))) s)
 
 class ConvertedToBits a where
     toBits :: a -> [Bool]
@@ -147,14 +105,9 @@ addLength len bytes =
     let (Byte64 (Byte32 byte7 byte6 byte5 byte4) (Byte32 byte3 byte2 byte1 byte0)) = integerToByte64 len
     in bytes ++ [byte3, byte2, byte1, byte0, byte7, byte6, byte5, byte4] -- little endian
 
-class CanModuloSum a where
-    (|+) :: [Bool] -> a -> [Bool]
-
-instance CanModuloSum [Bool] where
-    (|+) lhs rhs = manyBitsSum lhs rhs False []
-
-instance CanModuloSum Int where
-    (|+) lhs rhs = manyBitsSum lhs (toBits rhs) False []
+(|+) :: Byte32 -> Byte32 -> Byte32
+(|+) (Byte32 byte3 byte2 byte1 byte0) (Byte32 byte3' byte2' byte1' byte0')
+    = bools32ToByte32 $ manyBitsSum (bytesToBools [byte3, byte2, byte1, byte0]) (bytesToBools [byte3', byte2', byte1', byte0']) False []
 
 oneBitSum :: Bool -> Bool -> Bool -> (Bool, Bool)
 oneBitSum overflow a b =
@@ -171,31 +124,95 @@ manyBitsSum lhs rhs overflow acc | length acc == length lhs = acc
         (newOverflow, bit) = oneBitSum overflow a b
       in manyBitsSum lhs rhs newOverflow ([bit] ++ acc)
 
-shiftLMultiple :: [Bool] -> Int -> [Bool]
--- shiftLMultiple bits count | traceShow (fromBits bits, count) False = undefined
-shiftLMultiple bits count =
-    if count == 1
-    then shiftLOne bits
-    else shiftLMultiple (shiftLOne bits) (count - 1)
-  where shiftLOne (bit:bits) = bits ++ [bit]
+shiftLMultiple :: Byte32 -> Int -> Byte32
+shiftLMultiple (Byte32 byte3 byte2 byte1 byte0) count =
+    let
+        bitsInput = bytesToBools [byte3, byte2, byte1, byte0] 
+        shiftLOne (bit:bits) = bits ++ [bit]
+        shiftLMultiple' bits i = 
+            if i == 1
+            then shiftLOne bits
+            else shiftLMultiple' (shiftLOne bits) (i - 1)
+    in bools32ToByte32 $ shiftLMultiple' bitsInput count
 
-funF :: [Bool] -> [Bool] -> [Bool] -> [Bool]
--- funF xs ys zs | traceShow (fromBits xs, fromBits ys, fromBits zs, fromBits $ zipWith3 (\b c d -> (b && c)) xs ys zs) False = undefined
-funF xs ys zs = zipWith3 (\b c d -> (b && c) || ((not b) && d)) xs ys zs
+funF :: Byte32 -> Byte32 -> Byte32 -> Byte32
+funF b c d = (b `andByte32` c) `orByte32` ((notByte32 b) `andByte32` d)
 
-funG :: [Bool] -> [Bool] -> [Bool] -> [Bool]
-funG xs ys zs = zipWith3 (\b c d -> (d && b) || ((not d) && c)) xs ys zs
+funG :: Byte32 -> Byte32 -> Byte32 -> Byte32
+funG b c d = (d `andByte32` b) `orByte32` ((notByte32 d) `andByte32` c)
 
-funH :: [Bool] -> [Bool] -> [Bool] -> [Bool]
-funH xs ys zs = zipWith3 (\b c d -> (b `xor` c) `xor` d) xs ys zs
+funH :: Byte32 -> Byte32 -> Byte32 -> Byte32
+funH b c d = (b `xorByte32` c) `xorByte32` d
 
-funI :: [Bool] -> [Bool] -> [Bool] -> [Bool]
-funI xs ys zs = zipWith3 (\b c d -> c `xor` (b || not d)) xs ys zs
+funI :: Byte32 -> Byte32 -> Byte32 -> Byte32
+funI b c d = c `xorByte32` (b `orByte32` (notByte32 d))
 
 splitEvery :: Int -> [a] -> [[a]]
 splitEvery _ [] = []
 splitEvery n xs = as : splitEvery n bs 
   where (as,bs) = splitAt n xs
+
+bools8ToByte :: [Bool] -> Byte
+bools8ToByte x = let
+    bit0 = x!!7
+    bit1 = x!!6
+    bit2 = x!!5
+    bit3 = x!!4
+    bit4 = x!!3
+    bit5 = x!!2
+    bit6 = x!!1
+    bit7 = x!!0
+  in Byte bit7 bit6 bit5 bit4 bit3 bit2 bit1 bit0
+
+bytesToBools :: [Byte] -> [Bool]
+bytesToBools [] = []
+bytesToBools (Byte bit7 bit6 bit5 bit4 bit3 bit2 bit1 bit0 : xs) = [bit7, bit6, bit5, bit4, bit3, bit2, bit1, bit0] ++ bytesToBools xs
+
+bools32ToByte32 :: [Bool] -> Byte32
+bools32ToByte32 x = let
+    byte0 = bools8ToByte (drop 24 x)
+    byte1 = bools8ToByte (take 8 (drop 16 x))
+    byte2 = bools8ToByte (take 8 (drop 8 x))
+    byte3 = bools8ToByte (take 8 (x))
+  in Byte32 byte3 byte2 byte1 byte0
+
+bytesToBytes32 :: [Byte] -> [Byte32]
+bytesToBytes32 x = map (\xs -> bools32ToByte32 $ bytesToBools xs) (splitEvery 4 x)
+
+word8ToByte :: Word8 -> Byte
+word8ToByte x = bools8ToByte $ toBits x
+
+intToByte32 :: Int -> Byte32
+intToByte32 x = bools32ToByte32 $ toBits x
+
+byte32ToBytes :: Byte32 -> [Byte]
+byte32ToBytes (Byte32 a b c d) = [a, b, c, d]
+
+integerToByte64 :: Integer -> Byte64
+integerToByte64 x = let
+    bools = toBits x
+    small32 = bools32ToByte32 (drop 32 bools)
+    big32 = bools32ToByte32 (take 32 bools)
+  in Byte64 big32 small32
+
+bitOpByte :: (Bool -> Bool -> Bool) -> Byte -> Byte -> Byte
+bitOpByte op (Byte bit7 bit6 bit5 bit4 bit3 bit2 bit1 bit0) 
+    (Byte bit7' bit6' bit5' bit4' bit3' bit2' bit1' bit0')
+        = (Byte (bit7 `op` bit7') (bit6 `op` bit6') (bit5 `op` bit5') (bit4 `op` bit4') (bit3 `op` bit3') (bit2 `op` bit2') (bit1 `op` bit1') (bit0 `op` bit0'))
+
+bitOpByte32 :: (Byte -> Byte -> Byte) -> Byte32 -> Byte32 -> Byte32
+bitOpByte32 op (Byte32 byte3 byte2 byte1 byte0) (Byte32 byte3' byte2' byte1' byte0')
+    = (Byte32 (byte3 `op` byte3') (byte2 `op` byte2') (byte1 `op` byte1') (byte0 `op` byte0'))
+
+xorByte32 = bitOpByte32 (bitOpByte xor)
+orByte32  = bitOpByte32 (bitOpByte (||))
+andByte32 = bitOpByte32 (bitOpByte (&&))
+
+notByte :: Byte -> Byte
+notByte (Byte bit7 bit6 bit5 bit4 bit3 bit2 bit1 bit0) = (Byte (not bit7) (not bit6) (not bit5) (not bit4) (not bit3) (not bit2) (not bit1) (not bit0))
+
+notByte32 :: Byte32 -> Byte32
+notByte32 (Byte32 byte3 byte2 byte1 byte0) = Byte32 (notByte byte3) (notByte byte2) (notByte byte1) (notByte byte0)
 
 t = [
     0xd76aa478 :: Int, 0xe8c7b756, 0x242070db, 0xc1bdceee,
