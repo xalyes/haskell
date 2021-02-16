a30c6ea1dae74e984cce197187646ef2bc64eef4
xalyes <xalyes@gmail.com>
2021-02-17 02:05:51 +0300

    Throw out C8.pack-unpack

--- tcpcli.hs
+++ tcpcli.hs
@@ -14,12 +14,14 @@ import Control.Concurrent
 
 import Data.List
 import Data.Foldable
+import Data.List.Split
 
 import Text.Printf
 
 data Settings = Settings
   { path       :: String
-  , eps        :: Int }
+  , eps        :: Int 
+  , threads    :: Int } 
 
 settings :: Parser Settings
 settings = Settings
@@ -33,6 +35,11 @@ settings = Settings
          <> short 'e'
          <> help "How many Events Per Seconds send"
          <> metavar "NUM" )
+      <*> option auto
+          ( long "threads"
+         <> short 't'
+         <> help "How many thread use"
+         <> metavar "NUM" )
 
 main :: IO ()
 main = sendFile =<< execParser opts
@@ -42,25 +49,32 @@ main = sendFile =<< execParser opts
      <> progDesc "Send strings delimited by '\\n' from file to socket with some EPS")
 
 sendFile :: Settings -> IO ()
-sendFile (Settings path eps) = do events <- readFile path
-                                  start <- getCPUTime
-                                  runTCPClient "127.0.0.1" "13666" (handleSocket eps (processData eps events))
-                                  end <- getCPUTime
-                                  let diff = (fromIntegral (end - start)) / (10^12)
-                                  printf "Sending time: %0.3f sec\n" (diff :: Double)
+sendFile (Settings path eps threads) = do  events <- C.readFile path
+                                           --let chnks = processData eps events
+                                           --let chList = nChunks threads chunks
+                                           mVars <- sequence [newEmptyMVar | i <- [1..threads]]
+                                           --let threadData = zip mVars chList
+                                           start <- getCPUTime
+                                           sequence_ [forkIO $ do runTCPClient "127.0.0.1" "13666" (handleSocket [events]); putMVar mVar 1 | mVar <- mVars]
+                                           rs <- sequence [takeMVar mVar | mVar <- mVars]
+                                           end <- getCPUTime
+                                           let diff = (fromIntegral (end - start)) / (10^12)
+                                           printf "Sending time: %0.3f sec\n" (diff :: Double)
+                                           let resultEps = (fromIntegral $ (C.length $ C.filter (\e -> e == '\n') events) * threads) / diff
+                                           printf "EPS: %0.3f sec\n" (resultEps :: Double)
 
-handleSocket :: Int -> [String] -> Socket -> IO () 
-handleSocket eps (msg:rest) socket = do
-                                         start <- getCPUTime
+handleSocket :: [C.ByteString] -> Socket -> IO () 
+handleSocket (msg:rest) socket = do
+                                         --start <- getCPUTime
                                          -- let package = concatMap (\s -> if s == [] then "" else s ++ "\n") msg
-                                         sendAll socket (C.pack $ msg)
-                                         end <- getCPUTime
+                                         sendAll socket (msg)
+                                         --end <- getCPUTime
                                          if rest == [] then return () else do
-                                         let elapsed = end - start
-                                         if elapsed < 10^12 then do
-                                             threadDelay ((fromInteger (10^12 - elapsed)) `div` 10^6)
-                                             handleSocket eps rest socket
-                                         else handleSocket eps rest socket
+                                         --let elapsed = end - start
+                                         --if elapsed < 10^12 then do
+                                           --  threadDelay ((fromInteger (10^12 - elapsed)) `div` 10^6)
+                                            -- handleSocket rest socket
+                                         handleSocket rest socket
                                      
                                  
     -- msg <- recv s 1024
@@ -78,6 +92,10 @@ runTCPClient host port client = withSocketsDo $ do
     open addr = E.bracketOnError (openSocket addr) close $ \sock -> do
         connect sock $ addrAddress addr
         return sock
+
+nChunks n [] = []
+nChunks n l = let len = length l `div` n
+             in (take len l : nChunks (n-1) (drop len l))
 
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile
